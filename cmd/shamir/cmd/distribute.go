75a0416453ba49859baa81548af3a2e2e28b1673
Bryan Redd <bryan.d.redd@gmail.com>
2024-09-18 15:03:29 -0600

    Added QR code generation feature for easier share distribution

--- cmd/shamir/cmd/distribute.go
+++ cmd/shamir/cmd/distribute.go
@@ -8,6 +8,8 @@ import (
 
 	shamir "github.com/49pctber/shamir"
 	"github.com/spf13/cobra"
+
+	"github.com/skip2/go-qrcode"
 )
 
 var distributeCmd = &cobra.Command{
@@ -16,55 +18,88 @@ var distributeCmd = &cobra.Command{
 	Long:  `Distrbute a secret S into n shares, where any k shares can reconstruct S.`,
 }
 
+func parseInput(cmd *cobra.Command) (int, int, int, bool) {
+	invalid_command := false
+
+	nshares, err := cmd.Flags().GetInt("nshares")
+	if err != nil || nshares < 2 {
+		fmt.Println("provide n >= 2")
+		invalid_command = true
+	}
+
+	threshold, err := cmd.Flags().GetInt("threshold")
+	if err != nil || threshold < 2 {
+		fmt.Println("provide k >= 2")
+		invalid_command = true
+	}
+
+	primitivePoly, err := cmd.Flags().GetInt("primitive")
+	if err != nil {
+		fmt.Println("error reading primitive polynomial")
+		invalid_command = true
+	}
+
+	qr, _ := cmd.Flags().GetBool("qr")
+
+	if invalid_command {
+		os.Exit(1)
+	}
+
+	return nshares, threshold, primitivePoly, qr
+}
+
+func distributeQRCodes(s *shamir.Shamir) {
+	for _, share := range s.GetShares() {
+		fname, err := filepath.Abs(share.ShareLabel() + ".png")
+		if err != nil {
+			fmt.Printf("error saving QR code: %v\n", err)
+			break
+		}
+
+		err = qrcode.WriteFile(share.String(), qrcode.High, -5, fname)
+		if err != nil {
+			fmt.Printf("error saving QR code: %v\n", err)
+			break
+		}
+
+		fmt.Printf("saved QR code to %s\n", fname)
+	}
+}
+
+func generateSecret(secret []byte, primitivePoly, nshares, threshold int) *shamir.Shamir {
+	fmt.Printf("Generating %d-of-%d secret sharing scheme...\n", threshold, nshares)
+
+	s, err := shamir.NewShamirSecret(primitivePoly, nshares, threshold, secret)
+	if err != nil {
+		fmt.Printf("error distributing secret: %v\n", err)
+		os.Exit(1)
+	}
+
+	return s
+}
+
 var distributeFileCmd = &cobra.Command{
 	Use:   "file [filename]",
 	Short: "distributes a file",
 	Long:  ``,
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		invalid_command := false
 
-		nshares, err := cmd.Flags().GetInt("nshares")
-		if err != nil || nshares < 2 {
-			fmt.Println("provide n >= 2")
-			invalid_command = true
-		}
-
-		threshold, err := cmd.Flags().GetInt("threshold")
-		if err != nil || threshold < 2 {
-			fmt.Println("provide k >= 2")
-			invalid_command = true
-		}
-
-		primitivePoly, err := cmd.Flags().GetInt("primitive")
-		if err != nil {
-			fmt.Println("error reading primitive polynomial")
-			invalid_command = true
-		}
-
-		if invalid_command {
-			os.Exit(1)
-		}
-
-		secretstring, err := os.ReadFile(args[0])
-		if err != nil {
-			fmt.Println("error reading file")
-			os.Exit(1)
-		}
-
-		fmt.Printf("Generating %d-of-%d secret sharing scheme...\n", threshold, nshares)
-
-		s, err := shamir.NewShamirSecret(primitivePoly, nshares, threshold, []byte(secretstring))
-		if err != nil {
-			fmt.Printf("error distributing secret: %v\n", err)
-			os.Exit(1)
-		}
+		nshares, threshold, primitivePoly, qr := parseInput(cmd)
 
 		dir, err := os.Getwd()
 		if err != nil {
 			fmt.Printf("error getting current directory: %v\n", dir)
 			os.Exit(1)
 		}
+
+		secret, err := os.ReadFile(args[0])
+		if err != nil {
+			fmt.Println("error reading file")
+			os.Exit(1)
+		}
+
+		s := generateSecret(secret, primitivePoly, nshares, threshold)
 
 		for _, share := range s.GetShares() {
 
@@ -77,6 +112,10 @@ var distributeFileCmd = &cobra.Command{
 
 			fmt.Printf("%s saved to %s\n", share.ShareLabel(), fname)
 		}
+
+		if qr {
+			distributeQRCodes(s)
+		}
 	},
 }
 
@@ -87,42 +126,16 @@ var distributeStringCmd = &cobra.Command{
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 
-		invalid_command := false
-
-		nshares, err := cmd.Flags().GetInt("nshares")
-		if err != nil || nshares < 2 {
-			fmt.Println("provide n >= 2")
-			invalid_command = true
-		}
-
-		threshold, err := cmd.Flags().GetInt("threshold")
-		if err != nil || threshold < 2 {
-			fmt.Println("provide k >= 2")
-			invalid_command = true
-		}
-
-		primitivePoly, err := cmd.Flags().GetInt("primitive")
-		if err != nil {
-			fmt.Println("error reading primitive polynomial")
-			invalid_command = true
-		}
-
-		if invalid_command {
-			os.Exit(1)
-		}
-
 		secretstring := args[0]
+		nshares, threshold, primitivePoly, qr := parseInput(cmd)
+		s := generateSecret([]byte(secretstring), primitivePoly, nshares, threshold)
 
-		fmt.Printf("Generating %d-of-%d secret sharing scheme...\n", threshold, nshares)
-
-		s, err := shamir.NewShamirSecret(primitivePoly, nshares, threshold, []byte(secretstring))
-		if err != nil {
-			fmt.Printf("error distributing secret: %v\n", err)
-			os.Exit(1)
+		for _, share := range s.GetShares() {
+			fmt.Printf("%s\n", share.String())
 		}
 
-		for i := range nshares {
-			fmt.Printf("%s\n", s.ShareString(i))
+		if qr {
+			distributeQRCodes(s)
 		}
 	},
 }
@@ -132,6 +145,7 @@ func init() {
 	distributeCmd.PersistentFlags().IntP("nshares", "n", 0, "number of shares to produce")
 	distributeCmd.PersistentFlags().IntP("threshold", "k", 0, "the number of shares needed to reconstruct the secret")
 	distributeCmd.PersistentFlags().IntP("primitive", "p", 0x11d, "primitive polynomial to use when constructing Galois field")
+	distributeCmd.PersistentFlags().Bool("qr", false, "indicates QR codes of shares should be generated and saved in current directory")
 
 	distributeCmd.AddCommand(distributeFileCmd)
 
--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/49pctber/shamir
 
 go 1.22.3
 
-require github.com/spf13/cobra v1.8.1
+require (
+	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
+	github.com/spf13/cobra v1.8.1
+)
 
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
